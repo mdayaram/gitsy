b1a1f0b536bb480ebbc53c546cfda47892bb5ae5
Manoj Dayaram <noj@moovweb.com>
2012-11-28 04:19:21 -0800

    Added apollo check

---
+++ ext/apollo_check.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+#############################################
+# Arg List:
+#
+# [1]: User
+# [2]: Account/Project
+# [3]: R|W
+#
+
+require 'net/http'
+require 'uri'
+
+user = ARGV[0]
+account = ARGV[1].split("/")[0]
+project = ARGV[1].split("/")[1]
+perm = ARGV[2].upcase
+apollo_domain = "apollo-vc01"
+
+begin
+  url = "http://#{apollo_domain}/nosaka/git_access_check?user=#{user}&account=#{account}&project=#{project}&access=#{perm}"
+  response = Net::HTTP.get_response(URI.parse(url))
+rescue Exception => e
+  exit 2
+end
+
+if response.code != "200"
+  exit 1
+end
--- lib/checks/project.rb
+++ lib/checks/project.rb
@@ -4,7 +4,12 @@ module Gitsy
     class Project
 
       def self.check?(config, project, write)
-        return true
+        cmd = File.join(File.expand_path(File.dirname(__FILE__)), "../../ext", config.project_check)
+        cmd += " #{config.user} #{project}"
+        cmd += " W" if write
+        cmd += " R" if !write
+
+        Kernel.system cmd
       end
 
     end
