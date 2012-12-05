14f70f85649e0c5714fcd0cfe90d63188f7b457f
Manoj Dayaram <noj@moovweb.com>
2012-12-05 10:10:52 -0800

    The project check can now be an absolute path to a check script as well as a relative path to the ext directory.

--- lib/checks/project.rb
+++ lib/checks/project.rb
@@ -1,3 +1,4 @@
+require 'pathname'
 
 module Gitsy
   module Checks
@@ -6,7 +7,14 @@ module Gitsy
       def self.check?(config, project, write)
         return true if !config.project_check?
 
-        cmd = File.join(File.expand_path(File.dirname(__FILE__)), "../../ext", config.project_check)
+        cmd = config.project_check
+        if !Pathname.new(config.project_check).absolute?
+          cmd = File.join(
+            File.expand_path(File.dirname(__FILE__)),
+            "../../ext",
+            config.project_check)
+        end
+
         cmd += " #{config.user} #{project}"
         cmd += " W" if write
         cmd += " R" if !write
