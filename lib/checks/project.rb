ebbcf35b25a01ef33aea8ac4230fe4c0d466fcbb
Manoj Dayaram <noj@moovweb.com>
2013-01-28 16:19:53 -0800

    Making the project check adhere to the same config rules as custom commands

--- lib/checks/project.rb
+++ lib/checks/project.rb
@@ -7,20 +7,23 @@ module Gitsy
       def self.check?(env, project, write)
         return true if !env.config.project_check?
 
+        # If the path is not absolute, prepend the gitsy root path to it.
         cmd = env.config.project_check
         if !Pathname.new(cmd).absolute?
           cmd = File.join(env.gitsy_root, cmd)
         end
 
-        if File.exists? cmd
-          cmd += " #{env.config.user} #{project}"
-          cmd += " W" if write
-          cmd += " R" if !write
-          cmd += " >&2" # ensure all output goes to stderr.
-
-          return Kernel.system cmd
+        # If the file doesn't exist, maybe it's an executable in our PATH.
+        if !File.exists? cmd
+          cmd = env.config.project_check
         end
-        return false
+
+        cmd += " #{env.config.user} #{project}"
+        cmd += " W" if write
+        cmd += " R" if !write
+        cmd += " >&2" # ensure all output goes to stderr.
+
+        return Kernel.system cmd
       end
 
     end
